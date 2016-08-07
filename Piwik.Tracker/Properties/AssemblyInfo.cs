3117d3dac6da683feb9caadb1fff3e01a1fe7cdd
Julien Moumné <julien@piwik.org>
2016-08-07 15:32:12 +0200

    bump version to 2.16.0

--- Piwik.Tracker/Properties/AssemblyInfo.cs
+++ Piwik.Tracker/Properties/AssemblyInfo.cs
@@ -31,4 +31,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.14.0")]
+[assembly: AssemblyVersion("2.16.0")]
