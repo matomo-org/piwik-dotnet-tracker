matomo-org/piwik-dotnet-tracker

commit 4141d695a8f3e6625ab06478bfef59b21c5e52de

* renamed Url to PiwikBaseUrl and made PiwikBaseURL non static and mandatory in constructor

Piwik.Tracker/PiwikTracker.cs
@@ -136,7 +136,7 @@ namespace Piwik.Tracker
         /// Piwik base URL, for example http://example.org/piwik/
         /// Must be set before using the class by calling PiwikTracker.URL = 'http://yourwebsite.org/piwik/'
         /// </summary>
-        public static string Url;
+        public readonly string PiwikBaseUrl;
 
         /// <summary>
         /// Used in tests to output useful error messages.
@@ -204,8 +204,15 @@ namespace Piwik.Tracker
         /// </summary>
         /// <param name="siteId">Id site to be tracked</param>
         /// <param name="apiUrl">"http://example.org/piwik/" or "http://piwik.example.org/". If set, will overwrite PiwikTracker.URL</param>
-        public PiwikTracker(int siteId, string apiUrl = "")
+        public PiwikTracker(int siteId, string apiUrl)
         {
+            if (string.IsNullOrEmpty(PiwikBaseUrl))
+            {
+                throw new ArgumentException("You must first set the Piwik Tracker URL by calling PiwikTracker.URL = \"http://your-website.org/piwik/\";");
+            }
+            PiwikBaseUrl = apiUrl;
+            _siteId = siteId;
+
             _userAgent = null;
             _localTime = DateTimeOffset.MinValue;
             _hasCookies = false;
@@ -222,7 +229,6 @@ namespace Piwik.Tracker
             _ecommerceItems = new Dictionary<string, object[]>();
             _generationTime = null;
 
-            _siteId = siteId;
             var currentContext = HttpContext.Current;
             if (currentContext != null)
             {
@@ -244,7 +250,7 @@ namespace Piwik.Tracker
             _pageUrl = GetCurrentUrl();
             if (!string.IsNullOrEmpty(apiUrl))
             {
-                Url = apiUrl;
+                PiwikBaseUrl = apiUrl;
             }
 
             // Life of the visitor cookie (in sec)
@@ -788,7 +794,7 @@ namespace Piwik.Tracker
             }
 
             var postData = new JavaScriptSerializer().Serialize(data);
-            var response = SendRequest(GetBaseUrl(), "POST", postData, true);
+            var response = SendRequest(PiwikBaseUrl, "POST", postData, true);
 
             _storedTrackingActions = new List<string>();
 
@@ -1562,19 +1568,16 @@ namespace Piwik.Tracker
         /// <summary>
         /// Returns the base URL for the piwik server.
         /// </summary>
-        protected string GetBaseUrl()
+        /// <param name="url">The URL.</param>
+        /// <returns></returns>
+        private static string FixPiwikBaseUrl(string url)
         {
-            if (string.IsNullOrEmpty(Url))
-            {
-                throw new InvalidOperationException("You must first set the Piwik Tracker URL by calling PiwikTracker.URL = \"http://your-website.org/piwik/\";");
-            }
-            if (!Url.Contains("/piwik.php")
-                && !Url.Contains("/proxy-piwik.php")
+            if (!url.Contains("/piwik.php") && !url.Contains("/proxy-piwik.php")
             )
             {
-                Url += "/piwik.php";
+                url += "/piwik.php";
             }
-            return Url;
+            return url;
         }
 
         private string GetRequest(int idSite)
@@ -1590,7 +1593,7 @@ namespace Piwik.Tracker
                 }
             }
 
-            var url = GetBaseUrl() +
+            var url = PiwikBaseUrl +
                 "?idsite=" + idSite +
                 "&rec=1" +
                 "&apiv=" + Version +

Piwik.Tracker.Samples/PiwikTrackerSamples.cs
@@ -16,11 +16,11 @@ namespace Piwik.Tracker.Samples
     internal class PiwikTrackerSamples
     {
         private const string UA = "Firefox";
+        private static readonly string PiwikBaseUrl = "http://piwik.local";
+        private static readonly int SiteId = 1;
 
         private static void Main(string[] args)
         {
-            PiwikTracker.Url = "http://piwik.local";
-
             // ** PAGE VIEW **
             //            RecordSimplePageView();
             //            RecordSimplePageViewWithCustomProperties();
@@ -68,7 +68,7 @@ namespace Piwik.Tracker.Samples
         /// </summary>
         static private void GoalConversion()
         {
-            var piwikTracker = new PiwikTracker(1);
+            var piwikTracker = new PiwikTracker(SiteId, PiwikBaseUrl);
             piwikTracker.SetUserAgent(UA);
 
             var response = piwikTracker.DoTrackGoal(1, 42.69F);
@@ -81,7 +81,7 @@ namespace Piwik.Tracker.Samples
         /// </summary>
         static private void RecordCustomVariables()
         {
-            var piwikTracker = new PiwikTracker(1);
+            var piwikTracker = new PiwikTracker(SiteId, PiwikBaseUrl);
             piwikTracker.SetUserAgent(UA);
 
             piwikTracker.SetCustomVariable(1, "var1", "value1");
@@ -100,7 +100,7 @@ namespace Piwik.Tracker.Samples
         /// </summary>
         static private void RecordCustomDimensions()
         {
-            var piwikTracker = new PiwikTracker(1);
+            var piwikTracker = new PiwikTracker(SiteId, PiwikBaseUrl);
             piwikTracker.SetUserAgent(UA);
 
             piwikTracker.SetCustomTrackingParameter("dimension1", "value1");
@@ -116,7 +116,7 @@ namespace Piwik.Tracker.Samples
         /// </summary>
         static private void RecordSimplePageView()
         {
-            var piwikTracker = new PiwikTracker(1);
+            var piwikTracker = new PiwikTracker(SiteId, PiwikBaseUrl);
             piwikTracker.SetUserAgent(UA);
 
             var response = piwikTracker.DoTrackPageView("Document title of current page view");
@@ -129,7 +129,7 @@ namespace Piwik.Tracker.Samples
         /// </summary>
         static private void RecordSimplePageViewWithCustomProperties()
         {
-            var piwikTracker = new PiwikTracker(1);
+            var piwikTracker = new PiwikTracker(SiteId, PiwikBaseUrl);
             piwikTracker.SetUserAgent(UA);
 
             piwikTracker.SetResolution(1600, 1400);
@@ -164,7 +164,7 @@ namespace Piwik.Tracker.Samples
         /// </summary>
         static private void RecordSimplePageViewWithCustomGeoLocation()
         {
-            var piwikTracker = new PiwikTracker(1);
+            var piwikTracker = new PiwikTracker(SiteId, PiwikBaseUrl);
             piwikTracker.SetTokenAuth("XYZ");
 
             piwikTracker.SetUserAgent(UA);
@@ -185,7 +185,7 @@ namespace Piwik.Tracker.Samples
         /// </summary>
         static private void RecordSimplePageViewWithGenerationTime()
         {
-            var piwikTracker = new PiwikTracker(1);
+            var piwikTracker = new PiwikTracker(SiteId, PiwikBaseUrl);
             piwikTracker.SetUserAgent(UA);
 
             piwikTracker.SetGenerationTime(10000);
@@ -200,7 +200,7 @@ namespace Piwik.Tracker.Samples
         /// </summary>
         static private void GoalConversionWithAttributionInfo()
         {
-            var piwikTracker = new PiwikTracker(1);
+            var piwikTracker = new PiwikTracker(SiteId, PiwikBaseUrl);
             piwikTracker.SetUserAgent(UA);
 
             var attributionInfo = new AttributionInfo();
@@ -222,7 +222,7 @@ namespace Piwik.Tracker.Samples
         /// </summary>
         static private void TrackLink()
         {
-            var piwikTracker = new PiwikTracker(1);
+            var piwikTracker = new PiwikTracker(SiteId, PiwikBaseUrl);
             piwikTracker.SetUserAgent(UA);
 
             var response = piwikTracker.DoTrackAction("http://dev.piwik.org/svn", ActionType.Link);
@@ -235,7 +235,7 @@ namespace Piwik.Tracker.Samples
         /// </summary>
         static private void TrackDownload()
         {
-            var piwikTracker = new PiwikTracker(1);
+            var piwikTracker = new PiwikTracker(SiteId, PiwikBaseUrl);
             piwikTracker.SetUserAgent(UA);
 
             var response = piwikTracker.DoTrackAction("http://piwik.org/path/again/latest.zip", ActionType.Download);
@@ -248,7 +248,7 @@ namespace Piwik.Tracker.Samples
         /// </summary>
         static private void ECommerceCategoryView()
         {
-            var piwikTracker = new PiwikTracker(1);
+            var piwikTracker = new PiwikTracker(SiteId, PiwikBaseUrl);
             piwikTracker.SetUserAgent(UA);
 
             piwikTracker.SetEcommerceView("", "", new List<string> { "Electronics & Cameras" });
@@ -261,7 +261,7 @@ namespace Piwik.Tracker.Samples
         /// </summary>
         static private void ECommerceView()
         {
-            var piwikTracker = new PiwikTracker(1);
+            var piwikTracker = new PiwikTracker(SiteId, PiwikBaseUrl);
             piwikTracker.SetUserAgent(UA);
 
             piwikTracker.SetEcommerceView("SKU2", "PRODUCT name", new List<string> { "Electronics & Cameras", "Clothes" });
@@ -275,7 +275,7 @@ namespace Piwik.Tracker.Samples
         /// </summary>
         static private void ECommerceViewWithoutCategory()
         {
-            var piwikTracker = new PiwikTracker(1);
+            var piwikTracker = new PiwikTracker(SiteId, PiwikBaseUrl);
             piwikTracker.SetUserAgent(UA);
 
             piwikTracker.SetEcommerceView("SKU VERY nice indeed", "PRODUCT name");
@@ -289,7 +289,7 @@ namespace Piwik.Tracker.Samples
         /// </summary>
         static private void UpdateECommerceCartWithOneProduct()
         {
-            var piwikTracker = new PiwikTracker(1);
+            var piwikTracker = new PiwikTracker(SiteId, PiwikBaseUrl);
             piwikTracker.SetUserAgent(UA);
 
             piwikTracker.AddEcommerceItem(
@@ -309,7 +309,7 @@ namespace Piwik.Tracker.Samples
         /// </summary>
         static private void UpdateECommerceCartWithOneProductSKUOnly()
         {
-            var piwikTracker = new PiwikTracker(1);
+            var piwikTracker = new PiwikTracker(SiteId, PiwikBaseUrl);
             piwikTracker.SetUserAgent(UA);
 
             piwikTracker.AddEcommerceItem(
@@ -325,7 +325,7 @@ namespace Piwik.Tracker.Samples
         /// </summary>
         static private void UpdateECommerceCartWithMultipleProducts()
         {
-            var piwikTracker = new PiwikTracker(1);
+            var piwikTracker = new PiwikTracker(SiteId, PiwikBaseUrl);
             piwikTracker.SetUserAgent(UA);
 
             piwikTracker.AddEcommerceItem(
@@ -362,7 +362,7 @@ namespace Piwik.Tracker.Samples
         /// </summary>
         static private void RecordTwoECommerceOrders()
         {
-            var piwikTracker = new PiwikTracker(1);
+            var piwikTracker = new PiwikTracker(SiteId, PiwikBaseUrl);
             piwikTracker.SetUserAgent(UA);
 
             // First order
@@ -419,7 +419,7 @@ namespace Piwik.Tracker.Samples
 
         static private void RecordECommerceOrder()
         {
-            var piwikTracker = new PiwikTracker(1);
+            var piwikTracker = new PiwikTracker(SiteId, PiwikBaseUrl);
             piwikTracker.SetUserAgent(UA);
 
             piwikTracker.AddEcommerceItem(
@@ -453,7 +453,7 @@ namespace Piwik.Tracker.Samples
 
         private static void BulkTrackTwoRequests()
         {
-            var piwikTracker = new PiwikTracker(1);
+            var piwikTracker = new PiwikTracker(SiteId, PiwikBaseUrl);
 
             piwikTracker.SetUserAgent(UA);
             piwikTracker.SetTokenAuth("YOUR TOKEN");
@@ -468,7 +468,7 @@ namespace Piwik.Tracker.Samples
 
         static private void TrackSiteSearch()
         {
-            var piwikTracker = new PiwikTracker(1);
+            var piwikTracker = new PiwikTracker(SiteId, PiwikBaseUrl);
             piwikTracker.SetUserAgent(UA);
 
             var response = piwikTracker.DoTrackSiteSearch("keyword1", "category2", 0);
@@ -478,7 +478,7 @@ namespace Piwik.Tracker.Samples
 
         static private void TrackSongPlayback()
         {
-            var piwikTracker = new PiwikTracker(1);
+            var piwikTracker = new PiwikTracker(SiteId, PiwikBaseUrl);
             piwikTracker.SetUserAgent(UA);
 
             var response = piwikTracker.DoTrackEvent("music", "play", "Eye Of The Tiger");

Piwik.Tracker.Web.Samples/samples/AttributionInfoServerRead.aspx
@@ -1,22 +1,27 @@
 ﻿<%--http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later--%>
 
 <%@ Page Language="C#" %>
+
 <%@ Import Namespace="Piwik.Tracker" %>
 
-This page displays the result of the getAttributionInfo() method. <br/><br/>
+This page displays the result of the getAttributionInfo() method.
+<br />
+<br />
 
-If a tracking request has been sent via javascript, this method will output the content of the piwik ref cookie. <br/><br/>
+If a tracking request has been sent via javascript, this method will output the content of the piwik ref cookie.
+<br />
+<br />
 
-<% 
-    PiwikTracker.Url = ConfigurationSettings.AppSettings["PiwikURL"];
-    var piwikTracker = new PiwikTracker(1);
+<%
+    var PiwikBaseUrl = ConfigurationSettings.AppSettings["PiwikURL"];
+    var piwikTracker = new PiwikTracker(1, PiwikBaseUrl);
     piwikTracker.EnableCookies();
 
     var attributionInfo = piwikTracker.GetAttributionInfo();
-    
-    if(attributionInfo == null)
+
+    if (attributionInfo == null)
     {
-        Response.Write("No attribution information to read from cookies");                        
+        Response.Write("No attribution information to read from cookies");
     }
     else
     {
@@ -25,8 +30,7 @@ If a tracking request has been sent via javascript, this method will output the 
         Response.Write("referrerTimestamp : " + attributionInfo.ReferrerTimestamp + "<br>");
         Response.Write("referrerUrl : " + attributionInfo.ReferrerUrl + "<br>");
     }
-    
-    
+
 %>
 
 <br />

Piwik.Tracker.Web.Samples/samples/ServerSideDeleteCookies.aspx
@@ -1,14 +1,16 @@
 ﻿<%--http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later--%>
 
 <%@ Page Language="C#" %>
+
 <%@ Import Namespace="Piwik.Tracker" %>
 
+This page deletes all first party Piwik cookies using the Server Side Tracking API.
+<br />
+<br />
 
-This page deletes all first party Piwik cookies using the Server Side Tracking API. <br/><br/>
-
-<% 
-    PiwikTracker.Url = ConfigurationSettings.AppSettings["PiwikURL"];
-    var piwikTracker = new PiwikTracker(1);
+<%
+    var PiwikBaseUrl = ConfigurationSettings.AppSettings["PiwikURL"];
+    var piwikTracker = new PiwikTracker(1, PiwikBaseUrl);
     piwikTracker.DeleteCookies();
 %>
 

Piwik.Tracker.Web.Samples/samples/ServerSideGoalTracking.aspx
@@ -1,16 +1,19 @@
 ﻿<%--http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later--%>
 
 <%@ Page Language="C#" %>
+
 <%@ Import Namespace="Piwik.Tracker" %>
 <%@ Import Namespace="System.Net" %>
 <%@ Import Namespace="System.IO" %>
 
-This page tracks a goal conversion with the Server Side tracking API and displays the response. <br/><br/>
+This page tracks a goal conversion with the Server Side tracking API and displays the response.
+<br />
+<br />
 
-<% 
-    PiwikTracker.Url = ConfigurationSettings.AppSettings["PiwikURL"];
+<%
+    var PiwikBaseUrl = ConfigurationSettings.AppSettings["PiwikURL"];
 
-    var piwikTracker = new PiwikTracker(1);
+    var piwikTracker = new PiwikTracker(1, PiwikBaseUrl);
     piwikTracker.EnableCookies();
 
     var attributionInfo = new AttributionInfo();
@@ -21,7 +24,7 @@ This page tracks a goal conversion with the Server Side tracking API and display
     attributionInfo.ReferrerUrl = "http://www.example.org/test/really?q=yes";
 
     piwikTracker.SetAttributionInfo(attributionInfo);
-    
+
     piwikTracker.SetCustomVariable(1, "custom-variable1", "custom-variable1-value");
 
     var response = piwikTracker.DoTrackGoal(1, 42.69F);

Piwik.Tracker.Web.Samples/samples/VisitScopedCustomVariableServerRead.aspx
@@ -1,40 +1,43 @@
 ﻿<%--http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later--%>
 
 <%@ Page Language="C#" %>
+
 <%@ Import Namespace="Piwik.Tracker" %>
 
+This page displays visit scoped custom variables (index 1 & 2) using the Server Side Tracking API.
+<br />
+<br />
 
-This page displays visit scoped custom variables (index 1 & 2) using the Server Side Tracking API. <br/><br/>
-
-<% 
-    PiwikTracker.Url = ConfigurationSettings.AppSettings["PiwikURL"];
-    var piwikTracker = new PiwikTracker(1);
+<%
+    var PiwikBaseUrl = ConfigurationSettings.AppSettings["PiwikURL"];
+    var piwikTracker = new PiwikTracker(1, PiwikBaseUrl);
     piwikTracker.EnableCookies();
-    
+
     var customVar1 = piwikTracker.GetCustomVariable(1);
-    var customVar2 = piwikTracker.GetCustomVariable(2);   
-    
+    var customVar2 = piwikTracker.GetCustomVariable(2);
+
     if (customVar1 != null)
     {
-        Response.Write("Custom var index 1, name = " + customVar1.Name + "<br/>");
-        Response.Write("Custom var index 1, value = " + customVar1.Value + "<br/>");    
-    } else
-    {
-        Response.Write("No custom var on index 1 <br/>"); 
-    }
-
-    Response.Write("<br/>");
-
-    if (customVar2 != null)
-    {
-        Response.Write("Custom var index 2, name = " + customVar2.Name + "<br/>");
-        Response.Write("Custom var index 2, value = " + customVar2.Value + "<br/>");
+        Response.Write("Custom var index 1, name = " + customVar1.Name + "<br />");
+        Response.Write("Custom var index 1, value = " + customVar1.Value + "<br />");
     }
     else
     {
-        Response.Write("No custom var on index 2 <br/>");
-    }    
-                 
+        Response.Write("No custom var on index 1 <br />");
+    }
+
+    Response.Write("<br />");
+
+    if (customVar2 != null)
+    {
+        Response.Write("Custom var index 2, name = " + customVar2.Name + "<br />");
+        Response.Write("Custom var index 2, value = " + customVar2.Value + "<br />");
+    }
+    else
+    {
+        Response.Write("No custom var on index 2 <br />");
+    }
+
 %>
 
 <br />

commit 2697fe6cb4da8ae456422a945cf3e60053f8656f

* added test project with NUnit * fixed issue introduced in commit 4141d695a8f3e6625ab06478bfef59b21c5e52de (wrong variable test in constructor) * implemented fix for #37 * added test for GetCustomVariable()

Piwik.Tracker.Tests/Properties/AssemblyInfo.cs
@@ -1,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Piwik.Tracker.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Piwik.Tracker.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a6f87572-ab3d-477e-a97e-300dfed8cb5c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

Piwik.Tracker.Tests/packages.config
@@ -1,0 +1,4 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<packages>
+  <package id="NUnit" version="3.6.0" targetFramework="net45" />
+</packages>

Piwik.Tracker.sln
@@ -9,6 +9,8 @@ Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "Piwik.Tracker.Samples", "Pi
 EndProject
 Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "Piwik.Tracker.Web.Samples", "Piwik.Tracker.Web.Samples\Piwik.Tracker.Web.Samples.csproj", "{3D74EB7C-F2A4-49E4-8B2C-8247322F7DE0}"
 EndProject
+Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "Piwik.Tracker.Tests", "Piwik.Tracker.Tests\Piwik.Tracker.Tests.csproj", "{A6F87572-AB3D-477E-A97E-300DFED8CB5C}"
+EndProject
 Global
 	GlobalSection(SolutionConfigurationPlatforms) = preSolution
 		Debug|Any CPU = Debug|Any CPU
@@ -40,6 +42,14 @@ Global
 		{3D74EB7C-F2A4-49E4-8B2C-8247322F7DE0}.Release|Any CPU.Build.0 = Release|Any CPU
 		{3D74EB7C-F2A4-49E4-8B2C-8247322F7DE0}.Release|x86.ActiveCfg = Release|Any CPU
 		{3D74EB7C-F2A4-49E4-8B2C-8247322F7DE0}.Release|x86.Build.0 = Release|Any CPU
+		{A6F87572-AB3D-477E-A97E-300DFED8CB5C}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
+		{A6F87572-AB3D-477E-A97E-300DFED8CB5C}.Debug|Any CPU.Build.0 = Debug|Any CPU
+		{A6F87572-AB3D-477E-A97E-300DFED8CB5C}.Debug|x86.ActiveCfg = Debug|Any CPU
+		{A6F87572-AB3D-477E-A97E-300DFED8CB5C}.Debug|x86.Build.0 = Debug|Any CPU
+		{A6F87572-AB3D-477E-A97E-300DFED8CB5C}.Release|Any CPU.ActiveCfg = Release|Any CPU
+		{A6F87572-AB3D-477E-A97E-300DFED8CB5C}.Release|Any CPU.Build.0 = Release|Any CPU
+		{A6F87572-AB3D-477E-A97E-300DFED8CB5C}.Release|x86.ActiveCfg = Release|Any CPU
+		{A6F87572-AB3D-477E-A97E-300DFED8CB5C}.Release|x86.Build.0 = Release|Any CPU
 	EndGlobalSection
 	GlobalSection(SolutionProperties) = preSolution
 		HideSolutionNode = FALSE

Piwik.Tracker/PiwikTracker.cs
@@ -202,7 +202,7 @@ namespace Piwik.Tracker
         /// <exception cref="ArgumentException">apiUrl must not be null or empty</exception>
         public PiwikTracker(int idSite, string apiUrl)
         {
-            if (string.IsNullOrEmpty(PiwikBaseUrl))
+            if (string.IsNullOrEmpty(apiUrl))
             {
                 throw new ArgumentException("Piwik api url must not be emty or null.", nameof(apiUrl));
             }
@@ -360,24 +360,29 @@ namespace Piwik.Tracker
         public CustomVar GetCustomVariable(int id, Scopes scope = Scopes.Visit)
         {
             var stringId = Convert.ToString(id);
+            switch (scope)
+            {
+                case Scopes.Page:
+                    return _pageCustomVar.ContainsKey(stringId)
+                        ? new CustomVar(_pageCustomVar[stringId][0], _pageCustomVar[stringId][1])
+                        : null;
 
-            if (scope.Equals(Scopes.Page))
-            {
-                return _pageCustomVar.ContainsKey(stringId) ? new CustomVar(_pageCustomVar[stringId][0], _pageCustomVar[stringId][1]) : null;
+                case Scopes.Event:
+                    return _eventCustomVar.ContainsKey(stringId)
+                        ? new CustomVar(_eventCustomVar[stringId][0], _eventCustomVar[stringId][1])
+                        : null;
+
+                case Scopes.Visit:
+                    if (_visitorCustomVar.ContainsKey(stringId))
+                    {
+                        return new CustomVar(_visitorCustomVar[stringId][0], _visitorCustomVar[stringId][1]);
+                    }
+                    break;
+
+                default:
+                    throw new ArgumentException("Invalid 'scope' parameter value", nameof(scope));
             }
-            else if (!scope.Equals(Scopes.Event))
-            {
-                return _eventCustomVar.ContainsKey(stringId) ? new CustomVar(_eventCustomVar[stringId][0], _eventCustomVar[stringId][1]) : null;
-            }
-            else if (!scope.Equals(Scopes.Visit))
-            {
-                throw new ArgumentException("Invalid 'scope' parameter value", nameof(scope));
-            }
-            // todo: unreachable code, this will newer be called since we return before!
-            if (_visitorCustomVar.ContainsKey(stringId))
-            {
-                return new CustomVar(_visitorCustomVar[stringId][0], _visitorCustomVar[stringId][1]);
-            }
+
             var cookieDecoded = GetCustomVariablesFromCookie();
             if (!cookieDecoded.ContainsKey(stringId)
                 || cookieDecoded[stringId].Count() != 2)

Piwik.Tracker.Tests/Piwik.Tracker.Tests.csproj
@@ -1,0 +1,67 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<Project ToolsVersion="14.0" DefaultTargets="Build" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
+  <Import Project="$(MSBuildExtensionsPath)\$(MSBuildToolsVersion)\Microsoft.Common.props" Condition="Exists('$(MSBuildExtensionsPath)\$(MSBuildToolsVersion)\Microsoft.Common.props')" />
+  <PropertyGroup>
+    <Configuration Condition=" '$(Configuration)' == '' ">Debug</Configuration>
+    <Platform Condition=" '$(Platform)' == '' ">AnyCPU</Platform>
+    <ProjectGuid>{A6F87572-AB3D-477E-A97E-300DFED8CB5C}</ProjectGuid>
+    <OutputType>Library</OutputType>
+    <AppDesignerFolder>Properties</AppDesignerFolder>
+    <RootNamespace>Piwik.Tracker.Tests</RootNamespace>
+    <AssemblyName>Piwik.Tracker.Tests</AssemblyName>
+    <TargetFrameworkVersion>v4.5</TargetFrameworkVersion>
+    <FileAlignment>512</FileAlignment>
+  </PropertyGroup>
+  <PropertyGroup Condition=" '$(Configuration)|$(Platform)' == 'Debug|AnyCPU' ">
+    <DebugSymbols>true</DebugSymbols>
+    <DebugType>full</DebugType>
+    <Optimize>false</Optimize>
+    <OutputPath>bin\Debug\</OutputPath>
+    <DefineConstants>DEBUG;TRACE</DefineConstants>
+    <ErrorReport>prompt</ErrorReport>
+    <WarningLevel>4</WarningLevel>
+  </PropertyGroup>
+  <PropertyGroup Condition=" '$(Configuration)|$(Platform)' == 'Release|AnyCPU' ">
+    <DebugType>pdbonly</DebugType>
+    <Optimize>true</Optimize>
+    <OutputPath>bin\Release\</OutputPath>
+    <DefineConstants>TRACE</DefineConstants>
+    <ErrorReport>prompt</ErrorReport>
+    <WarningLevel>4</WarningLevel>
+  </PropertyGroup>
+  <ItemGroup>
+    <Reference Include="nunit.framework, Version=3.6.0.0, Culture=neutral, PublicKeyToken=2638cd05610744eb, processorArchitecture=MSIL">
+      <HintPath>..\packages\NUnit.3.6.0\lib\net45\nunit.framework.dll</HintPath>
+      <Private>True</Private>
+    </Reference>
+    <Reference Include="System" />
+    <Reference Include="System.Core" />
+    <Reference Include="System.Xml.Linq" />
+    <Reference Include="System.Data.DataSetExtensions" />
+    <Reference Include="Microsoft.CSharp" />
+    <Reference Include="System.Data" />
+    <Reference Include="System.Net.Http" />
+    <Reference Include="System.Xml" />
+  </ItemGroup>
+  <ItemGroup>
+    <Compile Include="PiwikTrackerTests.cs" />
+    <Compile Include="Properties\AssemblyInfo.cs" />
+  </ItemGroup>
+  <ItemGroup>
+    <None Include="packages.config" />
+  </ItemGroup>
+  <ItemGroup>
+    <ProjectReference Include="..\Piwik.Tracker\Piwik.Tracker.csproj">
+      <Project>{c3bb0e94-f45c-4691-81ff-061fc20e3209}</Project>
+      <Name>Piwik.Tracker</Name>
+    </ProjectReference>
+  </ItemGroup>
+  <Import Project="$(MSBuildToolsPath)\Microsoft.CSharp.targets" />
+  <!-- To modify your build process, add your task inside one of the targets below and uncomment it. 
+       Other similar extension points exist, see Microsoft.Common.targets.
+  <Target Name="BeforeBuild">
+  </Target>
+  <Target Name="AfterBuild">
+  </Target>
+  -->
+</Project>

Piwik.Tracker.Tests/PiwikTrackerTests.cs
@@ -1,0 +1,62 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Piwik.Tracker.Tests
+{
+    [TestFixture]
+    public class PiwikTrackerTests
+    {
+        private const string UA = "Firefox";
+        private static readonly string PiwikBaseUrl = "http://piwik.local";
+        private static readonly int SiteId = 1;
+
+        [Test]
+        [TestCase(Scopes.Page, null, null, null)]
+        [TestCase(Scopes.Page, 2, null, null)]
+        [TestCase(Scopes.Page, 2, "myPageVar", "myPageVarValue")]
+        [TestCase(Scopes.Event, null, null, null)]
+        [TestCase(Scopes.Event, 3, null, null)]
+        [TestCase(Scopes.Event, 3, "myEventVar", "myEventVarValue")]
+        [TestCase(Scopes.Visit, null, null, null)]
+        [TestCase(Scopes.Visit, 4, null, null)]
+        [TestCase(Scopes.Visit, 4, "myVisitVar", "myVisitVarValue")]
+        public void GetCustomVariable_Test(Scopes variableScope, int? variableId, string variableName, string variableValue)
+        {
+            //Arrange
+            var sut = new PiwikTracker(SiteId, PiwikBaseUrl);
+            if (variableId != null)
+            {
+                sut.SetCustomVariable(variableId.Value, variableName, variableValue, variableScope);
+            }
+            //Act
+
+            var actual = sut.GetCustomVariable(variableId ?? 99, variableScope);
+            //Assert
+            if (variableId != null)
+            {
+                Assert.That(actual.Name, Is.EqualTo(variableName));
+                Assert.That(actual.Value, Is.EqualTo(variableValue));
+            }
+            else
+            {
+                Assert.That(actual, Is.Null);
+            }
+        }
+
+        [Test]
+        public void GetCustomVariable_WhenScopeIsVisit_ReturnsVariableFromCookie()
+        {
+            Assert.That(false, Is.True, "Todo: Provide abillity to mock cookie!");
+        }
+
+        [Test]
+        public void GetCustomVariable_WhenInvalidScopeArgument_Throws()
+        {
+            //Arrange
+            var sut = new PiwikTracker(SiteId, PiwikBaseUrl);
+
+            //Act & Assert
+            Assert.Throws<ArgumentException>(() => sut.GetCustomVariable(1, (Scopes)1234));
+        }
+    }
+}