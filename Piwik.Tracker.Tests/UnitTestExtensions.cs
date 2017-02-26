3f79250b759305440f696cef41c5ce89e0ec8fb8
p.bruch <p.bruch@netbase.de>
2017-02-26 17:26:56 +0100

    added nuget FakeHttpContext (https://github.com/vadimzozulya/FakeHttpContext) for mocking cookie / HttpContext added nuget SimpleHttpMock (https://github.com/xiaoyvr/SimpleHttpMock) for mocking piwik server implemented several tests

---
+++ Piwik.Tracker.Tests/UnitTestExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+
+namespace Piwik.Tracker.Tests
+{
+    internal static class UnitTestExtensions
+    {
+        public static string GetAuthorityAndPath(this Uri uri)
+        {
+            return uri?.GetLeftPart(UriPartial.Path) ?? string.Empty;
+        }
+
+        public static IEnumerable<KeyValuePair<string, string>> ToKeyValuePairs(this NameValueCollection nameValueCollection)
+        {
+            return nameValueCollection.AllKeys.Select(k => new KeyValuePair<string, string>(k, nameValueCollection[(string)k]));
+        }
+    }
+}
