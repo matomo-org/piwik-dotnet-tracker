4bfdae491778cf3e7f8708af40ff52c597d98b44
Julien Moumné <julien@piwik.org>
2017-05-07 23:09:46 +0200

    chore: add script and hotshell menu for generating the changelog

---
+++ hs.js
@@ -0,0 +1,3 @@
+item({desc: 'piwik-dotnet-tracker'}, function() {
+  item({key: 'g', desc: 'generate changelog', cmd: './scripts/generate-changelog.sh'})
+})
---
+++ scripts/generate-changelog.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+github_changelog_generator --token $CHANGELOG_GITHUB_TOKEN --exclude-tags-regex "\-"
